5cd88c8ec045c8d2caa57db5c321da5442cc8ad7
Dytoma <dytomaomar@gmail.com>
2023-01-14 00:14:15 +0000

    first commit

---
+++ README-template.md
@@ -0,0 +1,64 @@
+# Frontend Mentor - Clipboard landing page solution
+
+This is a solution to the [Clipboard landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/clipboard-landing-page-5cc9bccd6c4c91111378ecb9). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./images/landing-page1.png)
+![](./images/landing-page2.png)
+
+Here are some screenshots of my solution, I couldn't take the entire page's screenshot because of the animations.
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- MBEM class notations
+- [GSAP](https://greensock.com/get-started/) - Animation library
+
+
+
+### Continued development
+
+GSAP is a powerful tool when it comes to animations while scrolling. It gives an smooth and amazing experience for users. I'm definetly gonna use is for my animated websites projects.
+
+### Useful resources
+
+- [Learn gsap scrollTrigger plugin](https://greensock.com/docs/v3/Plugins/ScrollTrigger) - This helped me for scrolling animations. I really liked this plugin and will use it going forward.
+
+
+## Author
+
+- Frontend Mentor - [@Dytoma](https://www.frontendmentor.io/profile/Dytoma)
+- Twitter - [@OmarDytoma](https://www.twitter.com/OmarDytoma)
+
---
+++ main.js
@@ -0,0 +1,277 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+tl.to(".text", { x: '0%', duration: 1, stagger: 0.25 });
+
+tl.to(".image-anim", { x: '0%', duration: 1 }, "-=1");
+
+tl.to(".slider", { y: '-100%', duration: 1.5, delay: 0.7});
+
+tl.to(".intro-animation", { y: "-100%", duration: 1}, "-=1");
+
+tl.to(".cta__logo img", {
+    scrollTrigger: {
+        animation: tl,
+        trigger: ".cta__logo img",
+        start: "top 70px",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    opacity: 0.5,
+    scale: 0.5,
+    duration: 2,
+    ease: "power1.out"
+})
+    .from(".cta__intro", {
+        scale: 0,
+        opacity: 0,
+        duration: 1
+    })
+    .from(".cta__description", {
+        opacity: 0,
+        duration: 1,
+    })
+    .from(".top-btn--primary", {
+        scrollTrigger: {
+            trigger: ".cta__intro",
+            start: "top 40%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-100%",
+        duration: 1,
+        opacity: 0
+    })
+    .from(".top-btn--secondary", {
+        scrollTrigger: {
+            trigger: ".cta__intro",
+            start: "top 40%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "100%",
+        duration: 1,
+        opacity: 0
+    }, "-=1")
+
+    tl.to(".about__intro", {
+        opacity: 1,
+        duration: 1,
+        ease: "power1.in"
+    });
+
+    tl.from(".about__description", {
+        scrollTrigger: {
+            trigger: ".about__text",
+            start: "top 50%",
+            end: "+=200px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        y: "10%",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.out"
+    })
+
+    tl.from(".features", {
+        scrollTrigger: {
+            trigger: ".about__description",
+            start: "top 40%",
+            end: "+=200px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "100%",
+        opacity: 0,
+        duration: 1.5,
+        stagger: 0.5,
+        ease: "bounce"
+    });
+
+    tl.from(".illustration-image", {
+        scrollTrigger: {
+            trigger: ".about__description",
+            start: "top 40%",
+            end: "+=200px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-100%",
+        opacity: 0,
+        duration: 1,
+        ease: 'linear'
+    }, "+=1")
+
+    tl.from(".benefits__intro", {
+        scrollTrigger: {
+            trigger: ".anim",
+            start: "top 40%",
+            end: "+=200px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        y: "-30%",
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out"
+    })
+
+    tl.from(".benefits__description", {
+        scrollTrigger: {
+            trigger: ".benefits__text",
+            start: "top 60%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        y: "20%",
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+    }, "+=1")
+
+    tl.from(".benefits__image img", {
+        scrollTrigger: {
+            trigger: ".benefits__text",
+            start: "top 40%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: '-100%',
+        opacity: 0,
+        duration: 1,
+        ease: 'slow'
+    })
+
+    tl.from(".functionalities__intro", {
+        scrollTrigger: {
+            trigger: ".benefits__image img",
+            start: "bottom 70%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        opacity: 0,
+        y: "-20%",
+        duration: 1,
+        ease: "power1.out"
+    })
+    tl.from(".functionalities__description", {
+        scrollTrigger: {
+            trigger: ".benefits__image img",
+            start: "bottom 70%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        opacity: 0,
+        y: "20%",
+        duration: 1,
+        ease: "power1.out"
+    }, "+=1")
+
+    tl.from(".functionality", {
+        scrollTrigger: {
+            trigger: ".benefits__image img",
+            start: "bottom 50%",
+            end: "+=300px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        y: "20%",
+        opacity: 0,
+        duration: 2,
+        stagger: 0.6,
+        ease: "power1.out"
+    })
+
+    tl.from(".logos img", {
+        scrollTrigger: {
+            trigger: ".functionalities",
+            start: "center 50%",
+            end: "+=300px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        opacity: 0,
+        duration: 2,
+        stagger: 0.6,
+        ease: "power1.out"
+    })
+
+    tl.from(".clipboard__text", {
+        scrollTrigger: {
+            trigger: ".logos",
+            start: "bottom 50%",
+            end: "+=100px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-100%",
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out"
+    })
+
+    tl.from(".bottom-btn--primary", {
+        scrollTrigger: {
+            trigger: ".clipboard__text h2",
+            start: "top 50%",
+            end: "+=300px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-100%",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.out"
+    })
+
+    tl.from(".bottom-btn--secondary", {
+        scrollTrigger: {
+            trigger: ".clipboard__text h2",
+            start: "top 50%",
+            end: "+=150px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "100%",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.out"
+    }, "+=1")
+
+    tl.from(".footer__logo img", {
+        scrollTrigger: {
+            trigger: ".bottom",
+            start: "top 60%",
+            end: "+=70px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-100%",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.out"
+    });
+
+    tl.from(".footer__links a", {
+        scrollTrigger: {
+            trigger: ".bottom",
+            start: "top 70%",
+            end: "+=70px",
+            toggleActions: "restart none none restart",
+            scrub: 1
+        },
+        x: "-30%",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.out",
+        stagger: 0.5,
+    }, "+=1")
